e7d5fa2e210fec981f50221d7cc5c3ac7d1cf1a8
Igor Karpov <igor.karpov@kstt.ua>
2019-04-23 17:53:34 +0300

    rotes file moved

--- application/routes/web.php
+++ application/routes/web.php
@@ -1,12 +1,3 @@
-<?php /**
-    *
-    * Copyright (c) 2019
-    * @package VMS - Video CMS v1.0
-    * @author Igor Karpov <ika@noxls.net>
-    * @author Sergey Karpov
-    * @website https://noxls.net
-    *
-*/?>
 <?php
 Route::middleware(['mustBeSubscribed'])->group(function () {
 
